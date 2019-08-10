86726fe26892846ce6c765ac35fc3aa3ae458e63
Qingrong Ke <keqingrong1992@gmail.com>
2019-08-10 20:44:26 +0800

    Add type definitions for AMap.plugin

---
+++ types/amap-js-api-plugin/index.d.ts
@@ -0,0 +1,59 @@
+
+declare namespace AMap {
+  /**
+   * https://lbs.amap.com/api/javascript-api/guide/abc/plugins
+   * https://lbs.amap.com/api/javascript-api/reference/plugin
+   */
+  type PluginName =
+    | 'AMap.ElasticMarker'
+    | 'AMap.ToolBar'
+    | 'AMap.Scale'
+    | 'AMap.OverView'
+    | 'AMap.MapType'
+    | 'AMap.Geolocation'
+    | 'AMap.AdvancedInfoWindow'
+    | 'AMap.Autocomplete'
+    | 'AMap.PlaceSearch'
+    | 'AMap.PlaceSearchLayer'
+    | 'AMap.DistrictSearch'
+    | 'AMap.LineSearch'
+    | 'AMap.StationSearch'
+    | 'AMap.Driving'
+    | 'AMap.TruckDriving'
+    | 'AMap.Transfer'
+    | 'AMap.Walking'
+    | 'AMap.Riding'
+    | 'AMap.DragRoute'
+    | 'AMap.ArrivalRange'
+    | 'AMap.Geocoder'
+    | 'AMap.CitySearch'
+    | 'AMap.IndoorMap'
+    | 'AMap.MouseTool'
+    | 'AMap.CircleEditor'
+    | 'AMap.PolyEditor'
+    | 'AMap.MarkerClusterer'
+    | 'AMap.RangingTool'
+    | 'AMap.CloudDataLayer'
+    | 'AMap.CloudDataSearch'
+    | 'AMap.Weather'
+    | 'AMap.RoadInfoSearch'
+    | 'AMap.Hotspot'
+    | 'AMap.Heatmap'
+
+  /**
+   * 加载插件
+   * @param pluginNames 插件名称
+   * @param callback 插件加载完成后的回调函数
+   */
+  function plugin(
+    pluginNames: PluginName | PluginName[],
+    callback: () => void
+  ): void
+
+  /**
+   * 加载服务
+   * 服务可以脱离地图单独使用，兼容原有的通过 AMap.plugin 方法加载的方式
+   * v1.3.5 (2014-12-18) 引入
+   */
+  const service: typeof plugin;
+}
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="./amap-js-api/index.d.ts" />
 /// <reference path="./amap-js-api-district-search/index.d.ts" />
 /// <reference path="./amap-js-api-geocoder/index.d.ts" />
+/// <reference path="./amap-js-api-plugin/index.d.ts" />
